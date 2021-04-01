aa4653395fd092ab246c527c83a71d1dbc036a71
PHG <73450249+HyeongGu0401@users.noreply.github.com>
2021-04-01 10:24:44 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       </div>
       <div className="Dev" style={{ fontFamily: "GongGothicMedium" }}>
         평범한 학생 프론트엔드 개발자입니다.
-        <div className="skill">기술 스택</div>
+        <div className="skill">사용할 수있는 </div>
       </div>
     </div>
   );
